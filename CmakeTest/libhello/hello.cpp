da75c796f5cf5f0418f02dec25715a30d58aa8d6
Yubin <wangyubin1989@gmail.com>
2014-12-31 10:11:20 +0800

    add cmakeExample

---
+++ CmakeTest/libhello/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "hello.h"
+using namespace std;
+
+void hello(const char *name)
+{
+	cout << "hello " << name << endl;
+	return;
+}
---
+++ CmakeTest/libhello/hello.h
@@ -0,0 +1,5 @@
+#ifndef YBWANG_HELLO_
+#define YBWANG_HELLO_
+
+void hello(const char* name);
+#endif
