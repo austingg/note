e01fb335c9b8764068fec023360faf6560b48416
austingg <wangyubin198905@gmail.com>
2016-08-25 22:04:16 +0800

    reorganize repository

--- tutorial/parse_name.py
+++
@@ -1,8 +0,0 @@
-# encoding:utf-8
-import os
-if __name__ == "__main__":
-    with open("star.csv") as f:
-    	with open('stars.txt', 'w') as outfile:
-	        for star in f.readlines():
-	            star_name = star.strip().split(",")
-	            outfile.write("%s\n" % star_name[0])
